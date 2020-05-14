95427864f55fd9149a22780025e66faee575d541
Osbert Lee <Osbert.Lee629@gmail.com>
2020-05-13 21:38:35 -0700

    add file

---
+++ Voice Bot/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Voice_Bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.Label();
+            this.state = new System.Windows.Forms.Label();
+            this.input = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.outputTextBox.Location = new System.Drawing.Point(313, 73);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(202, 209);
+            this.outputTextBox.TabIndex = 0;
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.inputTextBox.Location = new System.Drawing.Point(52, 73);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.ReadOnly = true;
+            this.inputTextBox.Size = new System.Drawing.Size(202, 209);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // output
+            // 
+            this.output.AutoSize = true;
+            this.output.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(353, 29);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(108, 33);
+            this.output.TabIndex = 3;
+            this.output.Text = "Output";
+            // 
+            // state
+            // 
+            this.state.AutoSize = true;
+            this.state.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.state.Location = new System.Drawing.Point(72, 310);
+            this.state.Name = "state";
+            this.state.Size = new System.Drawing.Size(104, 24);
+            this.state.TabIndex = 4;
+            this.state.Text = "State: On";
+            // 
+            // input
+            // 
+            this.input.AutoSize = true;
+            this.input.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input.Location = new System.Drawing.Point(100, 29);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(81, 33);
+            this.input.TabIndex = 5;
+            this.input.Text = "Input";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 363);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.state);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.outputTextBox);
+            this.Name = "Form1";
+            this.Text = "Voice Bot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label output;
+        private System.Windows.Forms.Label state;
+        private System.Windows.Forms.Label input;
+    }
+}
+
---
+++ Voice Bot/Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Diagnostics;
+using System.Media;
+
+namespace Voice_Bot
+{
+    public partial class Form1 : Form
+    {
+        //On/off
+        Boolean on = false;
+
+        //Grammar and Reponses
+        String[] grammarFile = (File.ReadAllLines(@"C:\Voice Bot\grammar.txt"));
+        String[] responseFile = (File.ReadAllLines(@"C:\Voice Bot\response.txt"));
+
+        //Speech Synth
+        SpeechSynthesizer speechSynth = new SpeechSynthesizer();
+
+        //Speech Rec
+        Choices grammarList = new Choices();
+        SpeechRecognitionEngine speechRecognition = new SpeechRecognitionEngine();
+
+        //my classes
+        String[] classesFile = (File.ReadAllLines(@"C:\Voice Bot\classes.txt"));
+        String[] zoomFile = (File.ReadAllLines(@"C:\Voice Bot\zoom.txt"));
+
+        public Form1()
+        {
+            this.WindowState = FormWindowState.Minimized;
+
+
+            //Init Grammar
+            grammarList.Add(grammarFile);
+            Grammar grammar = new Grammar(new GrammarBuilder(grammarList));
+
+            try
+            {
+                //RequestRecognizerUpdate updates
+                speechRecognition.RequestRecognizerUpdate();
+                speechRecognition.LoadGrammar(grammar);
+                //SpeechReconized raises when receives input that matches
+                speechRecognition.SpeechRecognized += rec_SpeechRecognized; 
+                speechRecognition.SetInputToDefaultAudioDevice(); // uses my audio device
+                speechRecognition.RecognizeAsync(RecognizeMode.Multiple); // say smth, it'll reset
+            }
+            catch {
+                
+                return;
+            }
+
+
+            //Custon Speech Synth Settings
+            speechSynth.SelectVoiceByHints(VoiceGender.Female);
+
+
+            InitializeComponent();
+        }
+
+        public void restart()
+        {
+            Application.Restart();
+        }
+
+        public void say(String text)
+        {
+            
+            speechSynth.SpeakAsync(text);
+            outputTextBox.AppendText(text + "\n");
+            on = false;
+        }
+
+
+        private void rec_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            String result = e.Result.Text;
+            int resp = Array.IndexOf(grammarFile, result); //find line number
+
+
+
+            if(responseFile[resp].IndexOf('*') == 0) //complex commands
+            {
+                if (result.Contains("hey")) //exit the program
+                {
+                    on = true;
+                    SystemSounds.Beep.Play();
+                }
+                
+
+                if (Process.GetProcessesByName("Discord").Length > 0 && result.Contains("deafen") && on) //deaf
+                {
+                    SendKeys.Send("\\");
+                    on = false;
+                    say(result);
+                }
+                
+                if (Process.GetProcessesByName("Discord").Length > 0 && result.Contains("mute") && on) //mute
+                {
+                    SendKeys.Send("{INSERT}");
+                    on = false;
+                    say(result);
+                }
+
+                if ((result.Contains("exit") || result.Contains("quit")) && on) //exit the program
+                {
+                    this.Close();
+                    return;
+                }
+
+                if (result.Contains("shut down") && on && state.Text.Contains("On")) //turn off
+                {
+                    say("good bye");
+                    on = false;
+                    state.Text = "State: Off";
+                }
+                if (result.Contains("activate") && state.Text.Contains("Off") && on) //activate
+                {
+                    say("I'm online");
+                    on = true;
+                    state.Text = "State: On";
+                }
+                if (result.Contains("restart")) //reboot
+                {
+                    say("rebooting");
+
+                    System.Threading.Thread.Sleep(1000); //let's the program say rebooting
+
+                    restart();
+                }
+                if (result.Contains("nevermind") && on && state.Text.Contains("On")) //activate
+                {
+                    say("Okay");
+                    on = false;
+                    
+                }
+
+                if (result.Contains("print screen") && on && state.Text.Contains("On")) //print screen
+                {
+                    
+                    SendKeys.Send("^%{PRTSC}");
+                    on = false;
+                    say(result);
+                }
+
+                if (Process.GetProcessesByName("DMT").Length > 0 && result.Contains("lock screen") && on && state.Text.Contains("On"))
+                {
+                    SendKeys.Send("+{DOWN}");
+                    on = false;
+                    say("locked");
+                } else if (result.Contains("lock screen") && on && state.Text.Contains("On"))
+                {
+                    Process.Start(@"C:\Users\Osbert\Desktop\dmt\DMT.exe");
+                    System.Threading.Thread.Sleep(2000);
+                    SendKeys.Send("+{DOWN}");
+                    on = false;
+                    say("locked");
+                }
+                
+                if (result.Contains("snipping tool") && on && state.Text.Contains("On")) // snipping
+                {
+                    SendKeys.Send("^%{UP}");
+                    on = false;
+                    say(result);
+                }
+                if (result.Contains("minimize") && on && state.Text.Contains("On")) //minimize
+                {
+                    this.WindowState = FormWindowState.Minimized;
+                    on = false;
+                    say("hiding");
+                }
+                if (result.Contains("come back") && on && state.Text.Contains("On")) //normal
+                {
+                    this.WindowState = FormWindowState.Normal;
+                    on = false;
+                    say("I'm back");
+                }
+                if (result.Contains("who") && on && state.Text.Contains("On")) //give date
+                {
+                    say("I was created by this asian male");
+                    Process.Start("http://linkedin.com/in/osblee/");
+
+                }
+                if (result.Contains("favorites") && on && state.Text.Contains("On")) //opens favorites
+                {
+                    Process.Start("http://youtube.com/");
+                    System.Threading.Thread.Sleep(500);
+                    Process.Start("http://kissanime.ru/");
+                    System.Threading.Thread.Sleep(500);
+                    Process.Start("http://kissmanga.com/");
+                    on = false;
+                    say("launching");
+                }
+                if (result.Contains("school") && on && state.Text.Contains("On")) //opens school
+                {
+                    Process.Start("https://courses.cs.washington.edu/courses/cse143/20sp/");
+                    System.Threading.Thread.Sleep(500);
+                    Process.Start("https://canvas.uw.edu/");
+                    
+                    on = false;
+                    say("launching");
+                }
+                if ((result.Contains("when is class") || result.Contains("go to class")) && on && state.Text.Contains("On")) //go to class
+                { // file should be formatted like so
+                  // Monday
+                  // (amount of classes)
+                  // 12 30 PM CSE LECTURE
+
+                    if (classesFile.Contains(DateTime.Now.ToString("dddd")))
+                    {
+                        int iDay = 0;
+                        int classNum = 0;
+                        for (int i = 0; i < classesFile.Length; i++)
+                        {
+                            if (DateTime.Now.ToString("dddd").Equals(classesFile[i]))
+                            {
+                                iDay = i;
+                                classNum = Int16.Parse(classesFile[i + 1]);
+                            }
+                        }
+
+                        string myClass = "";
+                        int iClass = iDay + 2;
+                        for (int i = 0; i < classNum; i++)
+                        {
+                            String[] classInfo = classesFile[iClass + i].Split(' ');
+
+                            int currentHour = DateTime.Now.Hour;
+                            int classHour = Int16.Parse(classInfo[0]);
+
+                            if (classInfo[2].Equals("PM") && classHour != 12)
+                            {
+                                classHour += 12;
+                            }
+                            //say("" + currentHour + " " + classHour + " " + DateTime.Now.Minute + " " + Int16.Parse(classInfo[1]));
+
+                            if (currentHour == classHour && DateTime.Now.Minute <= Int16.Parse(classInfo[1]))
+                            {
+                                say("Class starts in: " + (Int16.Parse(classInfo[1]) - DateTime.Now.Minute) + " minutes");
+                                myClass = classInfo[3] + " " + classInfo[4];
+                                break;
+                            }
+                            else if ((classHour - currentHour == 1) && ((60 - DateTime.Now.Minute) + Int16.Parse(classInfo[1]) < 60))
+                            {
+                                say("Class starts in: " + ((60 - DateTime.Now.Minute) + Int16.Parse(classInfo[1])) + " minutes");
+                                myClass = classInfo[3] + " " + classInfo[4];
+                                break;
+                            }
+                            else if ((currentHour < classHour) && (DateTime.Now.Minute > Int16.Parse(classInfo[1])))
+                            {
+                                say("Class starts in: " + (classHour - currentHour - 1) + " hours and " +
+                                        ((60 - DateTime.Now.Minute) + Int16.Parse(classInfo[1])) + " minutes");
+                                myClass = classInfo[3] + " " + classInfo[4];
+                                break;
+
+                            }
+                            else if ((currentHour < classHour) && (DateTime.Now.Minute < Int16.Parse(classInfo[1])))
+                            {
+                                say("Class starts in: " + (classHour - currentHour) + " hours and " +
+                                        (Int16.Parse(classInfo[1]) - DateTime.Now.Minute) + " minutes");
+                                myClass = classInfo[3] + " " + classInfo[4];
+                                break;
+                            }
+                        }
+
+                        if (myClass.Equals(""))
+                        {
+                            say("Done with classes today");
+                        }
+                        else
+                        {
+                            say(myClass);
+                            if (result.Contains("go to class"))
+                            {
+
+                                for (int i = 0; i < zoomFile.Length - 1; i++)
+                                {
+                                    if (myClass.Equals(zoomFile[i]))
+                                    {
+                                        say("Launching zoom");
+                                        Process.Start(zoomFile[i + 1]);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+
+
+                        
+                    }
+                    else
+                    {
+                        say("No class today");
+                    }
+
+                    on = false;
+
+                }
+
+
+
+                if (result.Contains("time") && on && state.Text.Contains("On")) //give time
+                {
+                    say(DateTime.Now.ToString(@"hh\:mm tt"));
+                }
+                if (result.Contains("date") && on && state.Text.Contains("On")) //give date
+                {
+                    say(DateTime.Now.ToString("dddd MMMM dd yyyy"));
+                }
+                
+            }
+            inputTextBox.AppendText(result + "\n");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
